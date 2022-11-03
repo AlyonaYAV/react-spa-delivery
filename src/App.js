0aed608176582028fdfd66cffda01c5c9c2ca9cf
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-03 18:36:53 +0200

    Base structure of the site with sections completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Header, Main, AboutUs, HowToApply, Faq, AboutUsSlider, Footer } from './sections';
+import { Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
+      <h1>React SPA delivery service</h1>
       <header className="App-header">
-        <h1>React SPA delivery service</h1>
+        <Header>
+          <Navbar />
+        </Header>
+        <Main />
+        <AboutUs />
+        <HowToApply />
+        <Faq />
+        <AboutUsSlider />
+        <Footer />
       </header>
     </div>
   );
