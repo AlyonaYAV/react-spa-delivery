ad56d6ba4a683e312730207894244d82520b6d38
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-03 18:27:04 +0200

    Created index.js file for conwinient export site sections

---
+++ src/sections/index.js
@@ -0,0 +1,7 @@
+export { default as AboutUs } from './about-us/AboutUs';
+export { default as AboutUsSlider } from './about-us-slider/AboutUsSlider';
+export { default as Faq } from './faq/Faq';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as HowToApply } from './how-to-apply/HowToApply';
+export { default as Main } from './main/Main';
