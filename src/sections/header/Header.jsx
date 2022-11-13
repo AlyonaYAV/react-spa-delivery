6c0b3025a5d20d88206afdf514a7f265e79b50fb
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-13 17:38:05 +0200

    Added animation to the site logo

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import './header.css';
+import logo from "./../../assets/Logo.png";
 
 const Header = (props)=>{
     return (<div className="header">
-        <div className="logo">Trafico.</div>
+        <div className="logo">
+            <img src={ logo } alt="Logo" width="35" height="35" />
+            <span>Trafico.</span>
+        </div>
         { props.children }
     </div>)
 }
