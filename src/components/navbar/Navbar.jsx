d09e0001577de18c94b1c1fb81c8cb73b1fc738c
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-13 20:17:12 +0200

    Here we use state and NavMenu component to work with adaptive navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import './navbar.css';
+import NavMenu from './../navmenu/NavMenu';
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
 
 const Navbar = ()=>{
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const toggleMenuBlock = ()=>{
+        setToggleMenu(s => !s);
+        console.log(toggleMenu);
+    }
+
     return (<nav className="navbar">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">How to apply</a></li>
-                <li><a href="#">FAQs</a></li>
-                <li><a href="#">About us slider</a></li>
-                <li><a href="#">Contact us</a></li>
-            </ul>
+            <NavMenu />
+            <div className="navbar__mobile-menu">
+                { toggleMenu ?
+                    <RiCloseLine color="rgb(66, 8, 4)" size={28} onClick={ toggleMenuBlock } />
+                    : <RiMenu3Line color="rgb(66, 8, 4)" size={28} onClick={ toggleMenuBlock } />
+                }
+                { toggleMenu && (<NavMenu />) }
+            </div>
         </nav>);
 }
 
