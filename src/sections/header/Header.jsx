add896fdfa05f68bedcf5fde7f2714b0a8d2afd9
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-21 20:14:16 +0200

    The div element of the header section is replaced with a header element

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -3,13 +3,13 @@ import './header.css';
 import logo from "./../../assets/Logo.png";
 
 const Header = (props)=>{
-    return (<div className="header">
+    return (<header className="header">
         <div className="logo">
             <img src={ logo } alt="Logo" width="35" height="35" />
             <span>Trafico.</span>
         </div>
         { props.children }
-    </div>)
+    </header>);
 }
 
 export default Header;
