f8f3a586dcb88018e5ad99b40387b76efecbd1ff
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-11-21 20:16:26 +0200

    The main section completed

--- src/sections/main/Main.jsx
+++ src/sections/main/Main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import './main.css';
+import { ImArrowRight2 } from 'react-icons/im';
 
 const Main = ()=>{
-    return (<div>
-        <h1>Main</h1>
+    return (<div className="main">
+        <div className="main__text">
+            <p>Your awesome traffic permit consultant.</p>
+            <button className="main__button">
+                Get started
+                <ImArrowRight2 className="main__button_arrow" />
+            </button>
+        </div>
     </div>)
 }
 
